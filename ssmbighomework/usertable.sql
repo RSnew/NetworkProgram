e582b47030f37f6de6913d0a42fe291b2ad1c34a
RSnew <RSnew@users.noreply.github.com>
2023-11-22 11:55:51 +0800

    RSnew second

--- ssmbighomework/usertable.sql
+++
@@ -1,9 +0,0 @@
-create table ssmbighomework.usertable
-(
-    userID   int auto_increment
-        primary key,
-    userName varchar(255) null,
-    password int          null,
-    isAdmin  tinyint(1)   null
-);
-
